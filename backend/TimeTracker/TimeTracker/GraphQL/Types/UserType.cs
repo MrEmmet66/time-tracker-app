ccbc28ca89f84641ecc58ce44d57c72583af74fd
MrEmmet66 <lorrenc7@gmail.com>
2024-07-27 21:06:28 +0300

    Change types

--- backend/TimeTracker/TimeTracker/GraphQL/Types/UserType.cs
+++ backend/TimeTracker/TimeTracker/GraphQL/Types/UserType.cs
@@ -9,9 +9,9 @@ public class UserType : ObjectGraphType<User>
     public UserType()
     {
         Field(x => x.Id, type: typeof(IdGraphType)).Description("User Id.");
-        Field(x => x.Email, type: typeof(IdGraphType)).Description("User email.");
-        Field(x => x.FirstName, type: typeof(IdGraphType)).Description("User first name.");
-        Field(x => x.LastName, type: typeof(IdGraphType)).Description("User last name.");
-        Field(x => x.Permissions, type: typeof(IdGraphType)).Description("User permissions.");
+        Field(x => x.Email, type: typeof(StringGraphType)).Description("User email.");
+        Field(x => x.FirstName, type: typeof(StringGraphType)).Description("User first name.");
+        Field(x => x.LastName, type: typeof(StringGraphType)).Description("User last name.");
+        Field<ListGraphType<StringGraphType>>(nameof(User.Permissions)).Description("User permissions.");
     }
 }
