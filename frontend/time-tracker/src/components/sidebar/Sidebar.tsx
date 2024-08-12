fb372232923940797650c427b5816e09eeb5c1b8
JokerMrx <renat.lakyziuk@gmail.com>
2024-08-12 22:07:31 +0300

    side menu items depend on user permissions

--- frontend/time-tracker/src/components/sidebar/Sidebar.tsx
+++ frontend/time-tracker/src/components/sidebar/Sidebar.tsx
@@ -1,11 +1,17 @@
+import {useMemo} from "react";
 import {UserOutlined, HomeOutlined, TeamOutlined} from "@ant-design/icons";
 import {Layout, Menu} from "antd";
-import {PAGES} from "../../constants/pages.constants";
 import {Link, useLocation} from "react-router-dom";
+
+import {PAGES} from "../../constants/pages.constants";
+import {useSelector} from "react-redux";
+import {RootState} from "../../redux/store";
+import {PERMISSIONS} from "../../constants/permissions.constants";
+import {userHasAccess} from "../../utils/user";
 
 const {Sider} = Layout;
 
-const menuItems = [
+const allMenuItems = [
     {
         key: PAGES.HOME,
         icon: <HomeOutlined/>,
@@ -15,16 +21,36 @@ const menuItems = [
         key: PAGES.USERS,
         icon: <UserOutlined/>,
         label: <Link to={PAGES.USERS}>Users</Link>,
+        permissions: [
+            PERMISSIONS.MANAGE_ALL_MEMBERS,
+            PERMISSIONS.MANAGE_TEAM_MEMBERS,
+        ],
     },
     {
         key: PAGES.TEAMS,
         icon: <TeamOutlined/>,
         label: <Link to={PAGES.TEAMS}>Teams</Link>,
+        permissions: [
+            PERMISSIONS.MANAGE_ALL_MEMBERS,
+            PERMISSIONS.MANAGE_TEAM_MEMBERS,
+        ],
     },
 ];
 
 const Sidebar = () => {
+    const {user} = useSelector((state: RootState) => state.auth);
     const location = useLocation();
+
+    const menuItems = useMemo(() => {
+        if (!user) {
+            return [allMenuItems[0]];
+        }
+
+        return allMenuItems.filter(
+            (item) =>
+                !item.permissions || userHasAccess(user.permissions, item.permissions)
+        );
+    }, [user]);
 
     return (
         <Sider theme="light" className="min-h-screen py-16" trigger={null}>
